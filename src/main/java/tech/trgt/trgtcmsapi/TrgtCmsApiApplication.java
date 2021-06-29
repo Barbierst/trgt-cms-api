7e01c23c85ed4f4a95102ff6347fa10f5d04ec44
Tim Barbiers <tim.barbiers@gmail.com>
2021-06-29 22:12:04 +0200

    Datamodel for entities

---
+++ src/main/java/tech/trgt/trgtcmsapi/TrgtCmsApiApplication.java
@@ -0,0 +1,13 @@
+package tech.trgt.trgtcmsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrgtCmsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrgtCmsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/trgt/trgtcmsapi/TrgtCmsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.trgt.trgtcmsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrgtCmsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
